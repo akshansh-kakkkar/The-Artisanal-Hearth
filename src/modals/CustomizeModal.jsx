8f87067a76fb894842ce5307fb206b844dfd252d
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-04-07 21:48:05 +0530

    still making the customizer modal

--- src/modals/CustomizeModal.jsx
+++ src/modals/CustomizeModal.jsx
@@ -1,12 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const CustomizeModal = ({ pizza, onClose, onAdd }) => {
   const [selectedPizzaSize, setSelectedPizzaSize] = useState(pizza.size?.[0]);
   const [selectedCrust, setSelectedCrust] = useState(pizza.crusts?.[0]);
   const [selectedToppings, setSelectedToppings] = useState([]);
-const toggleToppings = (toppings)=>{
-    setSelectedToppings((prev)=>prev.includes(toppings)?prev.filter((t)=> t !== toppings): [...prev, toppings])
-}
+  const toggleToppings = (toppings) => {
+    setSelectedToppings((prev) =>
+      prev.includes(toppings)
+        ? prev.filter((t) => t !== toppings)
+        : [...prev, toppings],
+    );
+  };
 
   const total =
     (selectedPizzaSize?.price || 0) +
@@ -14,13 +18,45 @@ const toggleToppings = (toppings)=>{
     selectedToppings.reduce((sum, t) => sum + t.price, 0);
   return (
     <>
-      <div onClick={()=> onClose()} className="fixed bg-black/50 inset-0 flex items-center justify-center">
-        <div onClick={(e)=>e.stopPropagation()} className="rounded-2xl z-100 bg-[#F6F3EE] w-[500px] h-[700px]">
-            <div className="flex justify-center overflow-hidden">
-                <img src={pizza.image} className="w-[500px] h-[300px] rounded-2xl object-cover scale-90" alt="" />
+      <div
+        onClick={() => onClose()}
+        className="fixed bg-black/50 inset-0 flex items-center justify-center"
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="rounded-2xl z-100 bg-[#F6F3EE] w-[500px] h-[700px]"
+        >
+          <div className="flex justify-between items-center mx-6 vietnam-font text-[#8F4E00] tracking-widest text-2xl items-center mt-3">
+            <div>BUILD YOUR MASTERPIECE</div>
+            <div
+              onClick={() => onClose()}
+              className="hover:scale-[85%] transition-all duration-300"
+            >
+              <img src="/assets/cross.svg" alt="" width={20} />
             </div>
+          </div>
+          <div className="flex justify-center overflow-hidden">
+            <img
+              src={pizza.image}
+              className="w-[500px] h-[300px] rounded-2xl object-cover scale-90"
+              alt=""
+            />
+          </div>
+          <div className="flex mx-7 justify-between text-center items-center">
+            <h2 className="flex justify-left  items-left heading-font text-[#AE131A]   text-2xl">
+              {pizza.name}
+            </h2>
+            <h2 className="flex justify-left  items-left  text-[#8F4E00] vietnam-font   text-2xl">
+              ₹ {pizza?.size?.[0]?.price}
+            </h2>
+          </div>
+          <div className="flex justify-center text-left ml-7 items-center  vietnam-font text-[#5B403D] text-sm">
+            {pizza.description}
+          </div>
+          <div className="mt-2">
+            <p className="font-semibold text-[#AE131A] mx-6 heading2-font text-md">Choose Crust</p>
+          </div>
         </div>
-          
       </div>
     </>
   );
